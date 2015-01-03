938fafd8eb10ef02f08f782f7ea21648ca72ac71
druch5 <druch5@tlen.pl>
2015-01-03 13:02:40 +0100

    Create help.lua

---
+++ help.lua
@@ -0,0 +1,26 @@
+--[[
+help
+@author ente91 <druch5@tlen.pl>
+@license GPLv2
+]]--
+
+local playerName = getAccountName ( getPlayerAccount ( thePlayer ) )	
+function mechanic1 ( thePlayer )
+ local playerName = getAccountName ( getPlayerAccount ( thePlayer ) )	
+  if isObjectInACLGroup ( "user." .. playerName, aclGetGroup ( "Admin" ) ) then
+	outputChatBox( "=====================================", getRootElement())
+    outputChatBox( "Spis Komend Baza Admina by ente91", getRootElement())
+	outputChatBox( "=====================================", getRootElement())
+	outputChatBox( "/loginadm - Teleportuje do bazy admina", getRootElement())
+	outputChatBox( "/admenter - Teleportuje do biura", getRootElement())
+	outputChatBox( "/admexit - Wyjscie z biura", getRootElement())
+	outputChatBox( "/admo - Otwieranie bramy", getRootElement())
+	outputChatBox( "/admc - Zamykanie bramy", getRootElement())
+	outputChatBox( "/admhelp - Spis komend", getRootElement())
+	outputChatBox( "=====================================", getRootElement())
+else
+
+outputChatBox( "Nie jestes członkiem administracji", getRootElement())
+end
+end
+addCommandHandler ( "admhelp", mechanic1 )
